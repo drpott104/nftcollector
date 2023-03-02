95e0b3615a9df4fc3730e5f3de6e9728cb63479e
apot <apotterton80@gmail.com>
2023-03-02 07:24:24 -0800

    part 1 '

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from django.db import reverse
 
 # Create your models here.
+class Nft(models.Model):
+    collection = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    attributes = models.TextField(max_length=250)
+    datePurchased = models.DateField()
+
+    def __str__(self):
+        return f'{self.collection} {self.name}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from .models import Nft
+
+nfts = [
+    {'name': '#9900', 'collection': 'Toxic Skulls Club', 'attributes': 'fumes, diamonds, trickster', 'date': 'Jan 18, 2023'},
+    {'name': '#7686', 'collection': 'Toxic Skulls Club', 'attributes': 'fumes, spikes, sailor', 'date': 'Nov 23, 2022'},
+]
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def nfts_index(request):
+    return render(request, 'nfts/index.html', {
+        'nfts': nfts
+    })
